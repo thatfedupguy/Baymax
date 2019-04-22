140c67a205d81cfa0131d1e52921ad494092c61e
tushar <tushardynasty@gmail.com>
2019-04-22 17:09:44 +0530

    Layout Created

---
+++ app/src/main/java/am/tk/baemax/MainActivity.java
@@ -0,0 +1,15 @@
+package am.tk.baemax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
